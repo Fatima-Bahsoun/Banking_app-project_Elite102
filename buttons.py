5975afbdc084f0581964358bed2c4e8aad570b46
student779xp <8085196@student.harmonytx.org>
2026-04-28 14:51:32 +0000

    "Added the deposit button; and change the .pack to .place"

--- buttons.py
+++ buttons.py
@@ -1,7 +1,10 @@
+#here_it is where I need to 
 #button to withdraw
 def my_buttons(underTheWindow):
     withdraw_button = tk.Button(underTheWindow, text="Withdraw", font=("Times New Roman", 18))#, width=259, height=96)#here: try the width to be 194
     #here: button not really correct sieze because its not by pixels
-    withdraw_button.pack(pady=10, padx=15)#PLACEHOLDER!! NOT IT
-    #frame is better??
-    
+    withdraw_button.place(y=70 x=20)#coordinates place holder
+#★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺#
+    deposite_button = tk.Button(underTheWindow, text="Deposite", font=("Times New Roman", 18))#, width=259, height=96)#here: try the width to be 194
+    #here_it fix sieze
+    deposite_button.place(y=120, padx=60)#here_it; this is a placehordder; Meaning testing is still needed
