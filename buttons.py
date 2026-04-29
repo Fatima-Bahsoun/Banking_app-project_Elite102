b5bf5202d1c7729e13fe0a4de10eff04d67e42d1
student779xp <8085196@student.harmonytx.org>
2026-04-29 13:49:31 +0000

    Added the blance function to the main.py and added as a button. ;D

--- buttons.py
+++ buttons.py
@@ -1,10 +1,19 @@
 #here_it is where I need to 
-#button to withdraw
+#|||See this to all
+#check all of the possitions 
+#, width=259, height=96)#here: try the width to be 194 |||
+#this is a function to be used in the banking app
 def my_buttons(underTheWindow):
-    withdraw_button = tk.Button(underTheWindow, text="Withdraw", font=("Times New Roman", 18))#, width=259, height=96)#here: try the width to be 194
+    withdraw_button = tk.Button(underTheWindow, text="Withdraw", font=("Times New Roman", 18))
     #here: button not really correct sieze because its not by pixels
     withdraw_button.place(y=70 x=20)#coordinates place holder
 #★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺#
-    deposite_button = tk.Button(underTheWindow, text="Deposite", font=("Times New Roman", 18))#, width=259, height=96)#here: try the width to be 194
+    deposite_button = tk.Button(underTheWindow, text="Deposite", font=("Times New Roman", 18))
     #here_it fix sieze
-    deposite_button.place(y=120, padx=60)#here_it; this is a placehordder; Meaning testing is still needed
+    deposite_button.place(y=70, padx=60)#here_it; this is a placehordder; Meaning testing is still needed
+#★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺#
+    account_button = tk.Button(underTheWindow, text="Account", font=("Times New Roman", 18))
+    account_button.place(y=120, padx=20)#here_it; still needs checking
+#★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺★☺#
+    balance_button = tk.Button(underTheWindow, text="Balance", font=("Times New Roman", 18))
+    balance_button.place(y=120, padx=60)
